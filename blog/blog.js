fbd9350a842b93d0f21df299a6c42e5729c40a76
AustinConner <a.l.conner.2002@gmail.com>
2025-02-15 22:03:30 -0700

    blog posts are now dynamically generated.

--- blog/blog.js
+++ blog/blog.js
@@ -23,5 +23,49 @@ const articles = [
 		ages: '12-16',
 		genre: 'Fantasy',
 		stars: '⭐⭐⭐⭐'
-	}
+	},
+	{
+		id: 3,
+		title: "Belgariad Book One: Pawn of Prophecy",
+		date: "Feb 12, 2022",
+		description:
+		"A fierce dispute among the Gods and the theft of a powerful Orb leaves the World divided into five kingdoms. Young Garion, with his 'Aunt Pol' and an elderly man calling himself Wolf --a father and daughter granted near-immortality by one of the Gods -- set out on a complex mission.",
+		imgSrc:
+		"https://images-na.ssl-images-amazon.com/images/I/41ZxXA+nInL.jpg",
+		imgAlt: "Book cover for Pawn of Prophecy",
+		ages: "12-16",
+		genre: "Fantasy",
+		stars: "⭐⭐⭐⭐⭐"
+		}
 ]
+
+// Where posts go
+var articleContainer = document.querySelector("#maincontent");
+
+// Create post HTML from the articles array
+function articleTemplate(article) {
+let articleTemplate = `
+<ul class="post-metadata">
+	<li class="date">${article.date}</li>
+	<li class="ages">${article.ages}</li>
+	<li class="genre">${article.genre}</li>
+	<li class="starts">${article.stars}</li>
+</ul>
+<div class="book-content">
+	<h1 class="book-title">${article.title}</h1>
+	<img class="book-image" src="${article.imgSrc}" alt="${articles[0].imgAlt}">
+	<p class="book-description">${article.description} <a class="cta" href="#">Read More...</a></p>
+</div>
+`
+createArticle(articleTemplate)
+
+}
+
+function createArticle(articleTemplate) {
+	const post = document.createElement('article');
+	post.classList.add("book-review");
+	post.innerHTML = articleTemplate;
+	articleContainer.appendChild(post);
+}
+
+articles.forEach(articleTemplate)
