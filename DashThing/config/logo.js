6d2e284385e9796f9e81d4eb507a08595222e972
AustinConner <a.l.conner.2002@gmail.com>
2025-04-05 13:30:11 -0600

    changed the logo destination in the config

--- DashThing/config/logo.js
+++ DashThing/config/logo.js
@@ -2,10 +2,10 @@
 // Specify the filepaths to the logo you want to use for the site.
 
 // Light Mode Logo
-export const lightLogo = "../assets/images/ui/logo.svg"
+export const lightLogo = "assets/images/ui/logo.svg"
 
 // Dark Mode Logo
-export const darkLogo = "../assets/images/ui/logo.svg"
+export const darkLogo = "assets/images/ui/logo.svg"
 
 // Logo alt tag
 export const logoAlt = "DashThing Logo"
