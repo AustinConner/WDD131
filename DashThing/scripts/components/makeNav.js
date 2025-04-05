1bb5d939be1083d2cf496620477d2f1f569e8dd7
AustinConner <a.l.conner.2002@gmail.com>
2025-04-05 03:23:01 -0600

    Icons are working now.

--- DashThing/scripts/components/makeNav.js
+++ DashThing/scripts/components/makeNav.js
@@ -1,51 +1,38 @@
 // MODULE
-// Generate boilerplate content on pages.
+// Generate boiler plate content on pages.
 import { pages } from "../../config/pages.js";
-import * as logo from "../../config/logo.js";
+import * as logo from "../../config/logo.js"
 
-document.addEventListener("DOMContentLoaded", () => {
-  // Create the nav structure
-  const mainNav = `
-    <nav id="main-nav">
-      <img id="logo" src="${logo.lightLogo}" alt="${logo.logoAlt}">
-      <ul id="nav-items"></ul>
-    </nav>
-  `;
-
-  // Insert the nav structure into the page
-  document.body.insertAdjacentHTML('afterbegin', mainNav);
-
-  // Add the navigation items dynamically
-  pages.forEach((item) => {
-    makeNavItems(item.tabName, item.lucideIcon, item.link, item.pageTitle);
-  });
-  
-  // Now that everything is done, remove the "loading" class to show the content
-  document.body.classList.remove("js-loading");
-});
-
-// Function to create nav items
 function makeNavItems(tab, icon, url, pageTitle) {
-  const navItem = document.createElement("li");
-  navItem.innerHTML = `
-    <a href="${url}">
-      <i data-lucide="${icon}"></i>${tab}
-    </a>
-  `;
-
-  // Mark the active page
-  const activePage = getActivePage();
-  if (String(activePage) === url) {
-    navItem.classList.add("active-nav");
-    document.title = pageTitle; // Dynamically update the page title
-  }
-
-  // Append nav item to the list
-  document.getElementById("nav-items").appendChild(navItem);
+    const navItem = document.createElement("li")
+    navItem.innerHTML = `
+    <a href=${url}><i data-lucide=${icon}></i>${tab}</a>
+    `
+    const activePage = getActivePage()
+    if (String(activePage) == url) { // The page that the user is on gets matched with that is in the config file.
+        console.log("Active Page:",activePage)
+        navItem.classList.add("active-nav")  // and set to be the active page on nav
+        document.title = pageTitle // update active page title
+    }
+    document.getElementById("nav-items").appendChild(navItem)
 }
 
-// Helper function to get the current page
+// Finds the page the user is on.
 function getActivePage() {
-  const currentPage = window.location.pathname.split("/").pop();
-  return currentPage;
+    const currentPage = window.location.pathname.split("/").pop();
+    return currentPage
 }
+
+// Create main nav container
+const mainNav = `
+<nav id="main-nav">
+  <img id="logo" src="${logo.lightLogo}" alt="${logo.logoAlt}">
+  <ul id="nav-items"></ul>
+</nav>
+`
+document.body.insertAdjacentHTML('afterbegin', mainNav)
+
+// Add items to navigation
+pages.forEach((item) => {
+    makeNavItems(item.tabName, item.lucideIcon, item.link, item.pageTitle)
+});
